5d6922f84da7d515a9f8cc6bed71ba7fe16c3097
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-07-24 10:38:58 +0300

    refactor: delete concole.log

--- AmastyPizza/db/configDB.php
+++ AmastyPizza/db/configDB.php
@@ -4,9 +4,7 @@ $username = "root";
 $password = "";
 $database = "amasty";
 
-
 $mysqli = new mysqli($hostname, $username, $password, $database);
-
 
 if ($mysqli->connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli->connect_error;
--- AmastyPizza/scripts/script.js
+++ AmastyPizza/scripts/script.js
@@ -9,11 +9,7 @@ $(document).ready(function () {
       success: function (response) {
         const invoiceElem = document.querySelector('#invoice__wrapper');
 
-        console.log(invoiceElem);
-
         invoiceElem.innerHTML = response;
-
-        console.log(response);
       },
     });
   });
